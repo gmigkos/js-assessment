cadc2ce63b64dd97032673dc07562fb8611d6cf4
gmigkos <gmigkos@gmail.com>
2013-09-04 15:16:42 +0300

    committed work done so far

--- app/functions.js
+++ app/functions.js
@@ -3,38 +3,72 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+      return fn.call(this,arr[0],arr[1],arr[2]);
     },
 
     speak : function(fn, obj) {
-
+      return fn.call(obj, obj.greeting, obj.name);
     },
 
     functionFunction : function(str) {
-
+       return function(str1) {
+        return str + ', ' + str1;
+      };
     },
 
     makeClosures : function(arr, fn) {
-
+      var a = [];
+      
+      var ret = function (arg){
+        return function() {
+          return fn(arg);
+        };
+      };
+      
+      for (var i=0; i<arr.length; i++){
+        a.push(ret(arr[i]));
+      }
+      
+    return a;
     },
 
     partial : function(fn, str1, str2) {
-
+      var args = Array.prototype.slice.call(arguments, 1)
+      return function() {
+        var remainingArgs = Array.prototype.slice.call(arguments)
+        return fn.apply(null, args.concat(remainingArgs))
+      }
     },
 
     useArguments : function() {
-
+      var args = Array.prototype.slice.call(arguments);
+      var ret = 0;
+      for(var i=0; i<args.length; i++){
+        ret+=args[i];
+      }
+      return ret;
     },
 
     callIt : function(fn) {
-
+      var args = Array.prototype.slice.call(arguments, 1);
+      return fn.apply(null, args);
     },
 
     partialUsingArguments : function(fn) {
-
+      var args = Array.prototype.slice.call(arguments, 1)
+      return function() {
+        var remainingArgs = Array.prototype.slice.call(arguments)
+        return fn.apply(null, args.concat(remainingArgs))
+        }
     },
 
     curryIt : function(fn) {
+    console.log(arguments);
+    var args = Array.prototype.slice.call(arguments, 1);
+    console.log(args);
+    return function() {
+      fn.apply(null, args);
+    };
 
     }
   };
--- app/modules.js
+++ app/modules.js
@@ -3,7 +3,17 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
-
+      var modName = str2,
+      modGreeting = str1,
+      retObj = {
+        name:modName,
+        greeting:modGreeting,
+        sayIt:function (){
+        return this.greeting+", "+this.name;
+        }  
+      };
+      console.log(retObj);
+      return retObj;
     }
   };
 });
