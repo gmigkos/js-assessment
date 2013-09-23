154e5aecc6b23df776677ec099ad7fb8b652bbe7
gmigkos <gmigkos@gmail.com>
2013-09-23 13:19:45 +0300

    Committed work done so far

--- app/async.js
+++ app/async.js
@@ -4,10 +4,25 @@ define([ 'jquery' ], function($) {
   return {
     async : function(value) {
 
+        var deferred = $.Deferred();
+
+        setTimeout(function() {
+            deferred.resolve(value);
+        }, 100);
+        return deferred.promise();
     },
 
     manipulateRemoteData : function(url) {
+        var deferred = $.Deferred();
 
+        $.getJSON(url,function(resp) {
+            var people = $.map (resp.people, function (i){
+                 return i.name;
+            });
+            deferred.resolve(people.sort());
+        });
+        console.log(deferred);
+        return deferred.promise();
     }
   };
 });
--- app/numbers.js
+++ app/numbers.js
@@ -3,19 +3,32 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+        var bin = num.toString(2);
+        if (bin.length < 8) {
+            bin = '0000000'.split('').slice( 0, 8 - bin.length ).join('') + bin;
+        }
+        var bitArr = bin.split('').map(Number);
+        return bitArr[8-bit];
     },
 
     base10: function(str) {
-
+        var num = 0;
+        for (var i = 0; i < str.length; i++){
+            num +=(Math.pow(2,7-i))*str[i];
+        }
+        return num;
     },
 
     convertToBinary: function(num) {
-
+        var bin = num.toString(2);
+        if (bin.length < 8) {
+            bin = '0000000'.split('').slice( 0, 8 - bin.length ).join('') + bin;
+        };
+        return bin;
     },
 
     multiply: function(a, b) {
-
+        return (a*10000000*(b*10000000))/(10000000*10000000);
     }
   };
 });
